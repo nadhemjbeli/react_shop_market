fc5c25356254e8b85c6f732320ba74f66b4fae46
nadhemjbeli <nadhem98jbeli@gmail.com>
2023-03-01 07:26:35 +0100

    atelier 2 partie 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,62 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import './App.css'
+import Test from "./Components/Test";
+import React from "react";
+import NavigationBar from "./Components/NavigationBar";
+import NotFound from "./Components/NotFound";
 
-import './App.css';
-import Products from './Components/Products';
+
+const ProductDetails =React.lazy (() => import('./Components/ ProductDetails'))
+const Products=React.lazy (() => import('./Components/Products'));
+
 
 function App() {
-  return (
-    <div>
-      <Products />
-    </div>
+ /* return (
+    <>
+    <BrowserRouter>
+
+    <NavigationBar />
+
+    <Routes>
+        <Route path="/home">
+          <Route index element={<Products />} />
+          <Route path=":name" element={<ProductDetails />}></Route>
+        </Route>
+        <Route path="/products" element={<Products />}></Route>
+        <Route path="/test/:username" element={<Test />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    
+
+    </BrowserRouter>
+      </>
+  );*/
+
+
+    return (
+    <>
+     <React.Suspense fallback={<h1> Loading</h1>} >
+          <NavigationBar />
+          <Routes>
+            <Route path="/home">
+              <Route index element={<Products />} />
+              <Route path=":name" element={<ProductDetails />}></Route>
+            </Route>
+            <Route path="/products" element={<Products />}></Route>
+            <Route path="/test/:username" element={<Test />}></Route>
+            <Route path="*" element={<NotFound />}></Route>
+          </Routes>
+          </React.Suspense>
+
+      </>
   );
+
+
+
+ 
+
+
 }
 
 export default App;
+
